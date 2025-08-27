cb9bc2bbce03241d9c830e362266e40f6795ed3e
BuBuck <sunjeongin123@gmail.com>
2025-08-27 15:25:59 +0900

    refactor: 메인과 프레임 분리

---
+++ src/main/SalaryLupin.java
@@ -0,0 +1,15 @@
+package main;
+
+import javax.swing.*;
+
+public class SalaryLupin extends JFrame {
+
+    public SalaryLupin() {
+
+        setTitle("월급 루팡");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        setSize(1600,900);
+        setLocationRelativeTo(null);
+    }
+}
